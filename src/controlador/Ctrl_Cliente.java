63bc65b1284607baf3dda44131c1061f532cb334
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-04-04 14:38:01 -0400

    Add files via upload

--- src/controlador/Ctrl_Cliente.java
+++ src/controlador/Ctrl_Cliente.java
@@ -1,6 +1,8 @@
 package controlador;
 
 import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -61,4 +63,114 @@ public class Ctrl_Cliente {
         }
         return respuesta;
     }
+    
+  //Metodo para guardar cliente
+    public void guardar(javax.swing.JTable table, javax.swing.JTextField textNombre, javax.swing.JTextField textApellido, javax.swing.JTextField textCedula, javax.swing.JTextField textTelefono, javax.swing.JTextField textDireccion) {
+    	if(textNombre.getText().isEmpty()|| textApellido.getText().isEmpty()|| textTelefono.getText().isEmpty()||
+				textCedula.getText().isEmpty()|| textDireccion.getText().isEmpty()){
+			
+			JOptionPane.showMessageDialog(null, "Complete todos los campos");
+			
+		}else {
+                            Ctrl_Cliente controlCliente = new Ctrl_Cliente();
+                            
+                                if(!controlCliente.clienteExistente(textCedula)){
+                                    
+                                    Cliente nuevoCliente = new Cliente();
+                                    nuevoCliente.setNombre(textNombre.getText().trim());
+                                    nuevoCliente.setApellido(textApellido.getText().trim());
+                                    nuevoCliente.setCedula(textCedula.getText().trim());
+                                    nuevoCliente.setTelefono(textTelefono.getText().trim());
+                                    nuevoCliente.setDireccion(textDireccion.getText().trim());
+                                    nuevoCliente.setEstado(1);
+                                
+                                    if(controlCliente.agregarCliente(nuevoCliente)){
+                                    
+                                        JOptionPane.showMessageDialog(null, "Cliente guardado");
+                                        textNombre.setText("");
+                                        textApellido.setText("");
+                                        textTelefono.setText("");
+                                        textCedula.setText("");
+                                        textDireccion.setText("");
+                                    }
+			
+                                }else{
+                                    
+                                    JOptionPane.showMessageDialog(null, "Este cliente ya existe");
+                                }
+                          
+		}
+    }
+    
+    /*
+     * @author Angel Miguel de la Rosa
+    
+   	 **********************************
+   	 * Metodo para actualizar cliente *
+   	 **********************************
+   	 */
+       public void actualizar(javax.swing.JTable table, javax.swing.JTextField textNombre, javax.swing.JTextField textApellido, javax.swing.JTextField textCedula, javax.swing.JTextField textTelefono, javax.swing.JTextField textDireccion) {
+           try {
+               Connection con = conexion.conectar();
+               int filaSeleccionada = table.getSelectedRow();
+
+               if (filaSeleccionada >= 0) {
+                   String nombre = textNombre.getText();
+                   String apellido = textApellido.getText();
+                   String cedula = textCedula.getText();
+                   String telefono = textTelefono.getText();
+                   String direccion = textDireccion.getText();
+
+                   String query = "UPDATE cliente SET nombre=?, apellido=?, cedula=?, telefono=?, direccion=? WHERE idCliente=?";
+                   PreparedStatement preparedStatement = con.prepareStatement(query);
+                   preparedStatement.setString(1, nombre);
+                   preparedStatement.setString(2, apellido);
+                   preparedStatement.setString(3, cedula);
+                   preparedStatement.setString(4, telefono);
+                   preparedStatement.setString(5, direccion);
+                   preparedStatement.setInt(6, (int) table.getValueAt(filaSeleccionada, 0)); 
+                   preparedStatement.executeUpdate();
+
+                   JOptionPane.showMessageDialog(null, "Cliente editado.");
+
+                   con.close();
+               } else {
+                   JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila para editar.", "Error", JOptionPane.ERROR_MESSAGE);
+               }
+           } catch (SQLException e1) {
+               System.out.println("Error al actualizar cliente: " + e1);
+               JOptionPane.showMessageDialog(null, "Error al actualizar cliente: " + e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+           }
+       }
+       
+       /*
+   	 ********************************
+   	 * Metodo para eliminar cliente *
+   	 ********************************
+   	 */
+       public void eliminar(javax.swing.JTable table) {
+           try {
+               Connection con = conexion.conectar();
+               int filaSeleccionada = table.getSelectedRow();
+
+               if (filaSeleccionada >= 0) {
+
+                   int idCliente = (int) table.getValueAt(filaSeleccionada, 0);
+
+                   String query = "DELETE FROM cliente WHERE idCliente=?";
+                   PreparedStatement preparedStatement = con.prepareStatement(query);
+                   preparedStatement.setInt(1, idCliente);
+                   preparedStatement.executeUpdate();
+
+                   JOptionPane.showMessageDialog(null, "Cliente eliminado.");
+
+                   con.close();
+               } else {
+                   JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila para eliminar.", "Error", JOptionPane.ERROR_MESSAGE);
+               }
+           } catch (SQLException e1) {
+               System.out.println("Error al eliminar cliente: " + e1);
+               JOptionPane.showMessageDialog(null, "Error al eliminar cliente: " + e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+           }
+       }
 }
--- src/controlador/Ctrl_Usuario.java
+++ src/controlador/Ctrl_Usuario.java
@@ -1,11 +1,14 @@
 package controlador;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import conexion.conexion;
 import javax.swing.JOptionPane;
+
+import Interfaz.stock;
 import modelo.Usuario;
 
 /**
@@ -36,4 +39,176 @@ public class Ctrl_Usuario {
          return respuesta;
     }
     
+    /*
+    * @author Angel Miguel de la Rosa
+    
+     **********************************
+     * Metodo para agregar un usuario *
+     **********************************
+     */
+    public boolean agregar(Usuario objeto){
+    	  boolean respuesta = false;
+    	  Connection cn = conexion.conectar();
+    	  
+    	  try {
+    		  
+    		  PreparedStatement consulta = cn.prepareStatement("insert into usuario value(?,?,?,?,?,?,?)");
+    		  consulta.setInt(1, 0);//ID
+    		  consulta.setString(2, objeto.getNombre());
+    		  consulta.setString(3, objeto.getApellido());
+    		  consulta.setString(4, objeto.getUsuario());
+    		  consulta.setString(5, objeto.getPassword());
+    		  consulta.setString(6, objeto.getTelefono());
+    		  consulta.setInt(7, objeto.getEstado());
+    		  
+    	  if (consulta.executeUpdate() > 0) {
+    		  respuesta = true;
+    		  
+    	  }
+    	  
+    	  cn.close();
+    	  
+    	  }catch (SQLException e) {
+    		  System.out.println("Error al guardar usuario" + e);
+    		  
+    	  }
+    	  
+		return respuesta;
+    }
+    
+    /*
+     *******************************************************************
+     * Metodo para revisar si el usuario ya esta reguitrado en la BBDD *
+     *******************************************************************
+     */
+    public boolean existeUsuario(String usuario){
+    	  boolean respuesta = false;
+    	  String sql = "select usuario from usuario where usuario = '" + usuario + "';";
+    	  Statement st;
+    	  
+    	  
+    	  try {
+    		  Connection cn = conexion.conectar();
+    		  st = cn.createStatement();
+    		  ResultSet rs = st.executeQuery(sql);
+    		  while (rs.next()) {
+				respuesta= true;
+				
+			}
+    	  
+    	  }catch (SQLException e) {
+    		  System.out.println("Error al consultar usuario" + e);
+    		  
+    	  }
+    	  
+		return respuesta;
+    }
+    
+    /*
+	 ******************************* 
+	 * Metodo para guardar usuario *
+	 *******************************
+	 */
+	public void guardar(javax.swing.JTextField textNombre, javax.swing.JTextField textApellido, javax.swing.JTextField textUser, javax.swing.JTextField textPassword, javax.swing.JTextField textTelefono) {
+		if(textNombre.getText().isEmpty()|| textApellido.getText().isEmpty()|| textTelefono.getText().isEmpty()||
+				textUser.getText().isEmpty()|| textPassword.getText().isEmpty()){
+			
+			JOptionPane.showMessageDialog(null, "Complete todos los campos");
+			
+		}else {
+			
+			//Verificar si el usuario ya está registrado
+			Usuario usuario =new Usuario();
+			Ctrl_Usuario ctrl_Usuario = new Ctrl_Usuario();
+			if(!ctrl_Usuario.existeUsuario(textUser.getText().trim())) {
+				
+				//Enviamos los datos del usuario
+				usuario.setNombre(textNombre.getText().trim());
+				usuario.setApellido(textApellido.getText().trim());
+				usuario.setUsuario(textUser.getText().trim());
+				usuario.setPassword(textPassword.getText().trim());
+				usuario.setTelefono(textTelefono.getText().trim());
+				usuario.setEstado(1);
+				
+				if (agregar(usuario)) {
+					JOptionPane.showMessageDialog(null, "Usuario registrado");
+				}else {
+					JOptionPane.showMessageDialog(null, "Error al registrar usuario");
+				}
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "El usuario ya está registrado, ingrese otro");
+			}
+			}
+		}
+	
+    /*
+	 ***********************************
+	 * Metodo para actualizar usuarios *
+	 ***********************************
+	 */
+    public void actualizar(javax.swing.JTable table, javax.swing.JTextField textNombre, javax.swing.JTextField textApellido, javax.swing.JTextField textUser, javax.swing.JTextField textPass, javax.swing.JTextField textTelefono) {
+        try {
+            Connection con = conexion.conectar();
+            int filaSeleccionada = table.getSelectedRow();
+
+            if (filaSeleccionada >= 0) {
+                String nombre = textNombre.getText();
+                String apellido = textApellido.getText();
+                String user = textUser.getText();
+                String pass = textPass.getText();
+                String telefono = textTelefono.getText();
+
+                String query = "UPDATE usuario SET nombre=?, apellido=?, usuario=?, password=?, telefono=? WHERE idUsuario=?";
+                PreparedStatement preparedStatement = con.prepareStatement(query);
+                preparedStatement.setString(1, nombre);
+                preparedStatement.setString(2, apellido);
+                preparedStatement.setString(3, user);
+                preparedStatement.setString(4, pass);
+                preparedStatement.setString(5, telefono);
+                preparedStatement.setInt(6, (int) table.getValueAt(filaSeleccionada, 0)); 
+                preparedStatement.executeUpdate();
+
+                JOptionPane.showMessageDialog(null, "Usuario editado.");
+
+                con.close();
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila para editar.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (SQLException e1) {
+            System.out.println("Error al actualizar usuario: " + e1);
+            JOptionPane.showMessageDialog(null, "Error al actualizar usuario: " + e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+    /*
+	 *********************************
+	 * Metodo para eliminar usuarios *
+	 *********************************
+	 */
+    public void eliminar(javax.swing.JTable table) {
+        try {
+            Connection con = conexion.conectar();
+            int filaSeleccionada = table.getSelectedRow();
+
+            if (filaSeleccionada >= 0) {
+
+                int idUsuario = (int) table.getValueAt(filaSeleccionada, 0);
+
+                String query = "DELETE FROM usuario WHERE idUsuario=?";
+                PreparedStatement preparedStatement = con.prepareStatement(query);
+                preparedStatement.setInt(1, idUsuario);
+                preparedStatement.executeUpdate();
+
+                JOptionPane.showMessageDialog(null, "Usuario eliminado.");
+
+                con.close();
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor, seleccione una fila para eliminar.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (SQLException e1) {
+            System.out.println("Error al eliminar usuario: " + e1);
+            JOptionPane.showMessageDialog(null, "Error al eliminar usuario: " + e1.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
