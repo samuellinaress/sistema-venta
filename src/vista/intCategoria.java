eb90829bfa79a374cf4f9b1ba53f9397d402dcab
AngelNaut <118201609+AngelNaut@users.noreply.github.com>
2024-04-07 19:05:44 -0400

    Add files via upload

--- src/vista/intCategoria.java
+++ src/vista/intCategoria.java
@@ -1,12 +1,20 @@
 package vista;
-
+//Autor: Angel Naut;
 import java.awt.EventQueue;
 
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
+
+import controlador.Ctrl_Categoria;
+import modelo.Categoria;
+
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class intCategoria extends JInternalFrame {
 
@@ -35,6 +43,42 @@ public class intCategoria extends JInternalFrame {
 		desCategoria.setColumns(10);
 		
 		JButton guardar = new JButton("Guardar");
+		guardar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Categoria categoria = new Categoria();
+				Ctrl_Categoria controlCategoria = new Ctrl_Categoria();
+
+				// validamos campos vacios
+				if (desCategoria.getText().isEmpty()) {
+
+					JOptionPane.showMessageDialog(null, "Completa todos los campos");
+
+				} else {
+
+					if (!controlCategoria.existeCategoria(desCategoria.getText().trim())) {
+						categoria.setDescripcion(desCategoria.getText().trim());
+						categoria.setEstado(1);
+
+						if (controlCategoria.guardar(categoria)) {
+
+							JOptionPane.showMessageDialog(null, "Resgistro Guardado");
+
+						} else {
+
+							JOptionPane.showMessageDialog(null, "Error al Guardar");
+
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "La categoria ya esta guardada");
+					}
+
+				}
+
+				// limpiar campo
+
+				desCategoria.setText("");
+			}
+		});
 		guardar.setFont(new Font("Tahoma", Font.BOLD, 14));
 		guardar.setBounds(192, 102, 109, 30);
 		getContentPane().add(guardar);
