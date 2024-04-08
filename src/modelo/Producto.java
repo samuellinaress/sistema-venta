aa289772ee6dd82f8185e76bedbdbb2435c454cc
Angel27Miguel <165101793+Angel27Miguel@users.noreply.github.com>
2024-04-08 13:49:41 -0400

    Add files via upload

---
+++ src/modelo/Producto.java
@@ -0,0 +1,117 @@
+package modelo;
+/*
+ * @author Angel Miguel de la Rosa
+ * */
+
+import vista.intCategoria;
+
+public class Producto {
+
+	//Atributos 
+	private int idProducto;
+	private String nombre;
+	private int cantidad;
+	private double precio;
+	private String descripcion;
+	private int itbis;
+	private int idCategoria;
+	private int estado;
+	
+	//Costructor
+	
+	public Producto() {
+		this.idProducto=0;
+		this.nombre = "";
+		this.cantidad = 0;
+		this.precio = 0.0;
+		this.descripcion = "";
+		this.itbis = 0;
+		this.idCategoria = 0;
+		this.estado = 0;
+	}
+
+	//Costructor sobrecargado
+	
+	public Producto(int idProducto, String nombre, int cantidad, double precio, String descripcion, int itbis,
+			int idCategoria, int estado) {
+		super();
+		this.idProducto = idProducto;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.descripcion = descripcion;
+		this.itbis = itbis;
+		this.idCategoria = idCategoria;
+		this.estado = estado;
+	}
+
+	//Metodo set and get 
+	
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getItbis() {
+		return itbis;
+	}
+
+	public void setItbis(int itbis) {
+		this.itbis = itbis;
+	}
+
+	public int getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(int idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public int getEstado() {
+		return estado;
+	}
+
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+
+	
+	
+	
+}
